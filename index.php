3d56b8b90769c4d19c9aeaf338689423e683e562
Vaibhav Agarwal <vaibhavagarwal220@gmail.com>
2017-03-09 23:08:44 +0530

    fixed some design flaws

--- index.php
+++ index.php
@@ -1,22 +1,67 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>Dashboard</title>
-<?php include 'include.inc.php';?></head>
-<body>
-<center>
-  
-  <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href=admin/adminloginpage.php>
-  ADMIN
-</a>
-<a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href=students/studentlogin.php>
-  STUDENT
-</a>
-<a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" href="faculty/facultylogin.php">
-  FACULTY
-</a>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/favicon.ico">
 
-</center>
- 
-</body>
+    <title>Dashboard</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/custom-animations.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+    	#h{
+    		background-image: url("assets/img/head.jpg");
+    	}
+    </style>
+  </head>
+
+  <body>
+
+
+	<div id="h">
+		<div class="container">
+			<div class="row">
+			    		    <li><a class="btn btn-lg btn-info btn-register" href=admin/adminloginpage.php>Admin Login</a>
+			   <a class="btn btn-lg btn-info btn-register" href=students/studentlogin.php>Student Login</a>
+			   <a class="btn btn-lg btn-info btn-register" href=faculty/facultylogin.php>Faculty Login</a></li>
+	
+			    <div class="col-md-10 col-md-offset-1 mt">
+			    	
+			    	<h1 class="mb">STUDENT DASHBOARD</h1>
+			    	<h3>A platform for all students of IIT Mandi</h3>
+			    </div>
+			    <div class="col-md-12 mt hidden-xs">
+			    	<img src="assets/img/graph1.PNG" class="img-responsive aligncenter" alt="" data-effect="slide-bottom">
+			    </div>
+			</div>
+		</div>
+	</div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/retina-1.1.0.js"></script>
+    <script src="assets/js/jquery.unveilEffects.js"></script>
+  </body>
 </html>
